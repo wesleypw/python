03ffa5e6cf48f730c95c44644793a288b16688a4
wesley henrique silva da silva <wesleyhsspw@gmail.com>
2025-05-25 23:52:19 -0300

    essa pasta é dedicada aos meus estudos em python

---
+++ ex1.py
@@ -0,0 +1,14 @@
+cpf = input("Digite o CPF: ")
+while True:
+    if cpf == 11 or cpf == 14:
+        print("CPF válido",cpf)
+        break
+    else:
+        print("CPF inválido")
+        break
+    
+    
+ 
+
+
+
