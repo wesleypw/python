f5fe4a436c83bfa5785c0fd66e66277fef9fec4d
wesley henrique silva da silva <wesleyhsspw@gmail.com>
2025-06-01 18:19:47 -0300

    tratamento de cpf e validação de cpf

--- ex1.py
+++ ex1.py
@@ -1,16 +1,25 @@
 cpf = input("Digite o CPF: ")
 
-while True:
+def validacao_cpf():
+    tratar = tratar_cpf(cpf)
+    tratar = cpf 
+
+    num = list(range(1,11))
+    for i in num:
+        for j in tratar:
+            mult = i * j
+            print(mult)    
+
+def tratar_cpf(cpf):
     if len(cpf) == 11 or len(cpf) == 14:
         if len(cpf) == 14:
-            cpf = cpf.replace(".", "-")
-            print(cpf)
-            break
-           
-    else:
-        print("CPF inválido")
-        break
+            cpf14 = int(cpf.replace('.', '').replace('-', ''))    
+        elif len(cpf) == 11:
+            cpf11 = int(cpf)
+            cpf = cpf11, cpf14
+        
 
+validacao_cpf()
 
     
 
